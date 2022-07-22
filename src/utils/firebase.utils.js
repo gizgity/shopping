c00b885a65eaa4a52f51386dca0262288cddde23
gizgity <huutriks.96@gmail.com>
2022-07-22 10:52:35 +0700

    add google popup and redirect sign in

---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmmAqeOVyiWYar3_dwL4K-vTpTMtK5tkI",
+  authDomain: "crwn-shopping-page-12fe7.firebaseapp.com",
+  projectId: "crwn-shopping-page-12fe7",
+  storageBucket: "crwn-shopping-page-12fe7.appspot.com",
+  messagingSenderId: "155944139743",
+  appId: "1:155944139743:web:b50beca065c47927b3a26a",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "user", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    //check if user exist, create user if not
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (e) {
+      console.log("error creating the user", e.message);
+    }
+  }
+  return userDocRef;
+};
